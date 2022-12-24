2fe11920e6dec97f9c317fac319cc19563a940f7
pocochu <pococho12345@gmail.com>
2022-12-24 03:43:05 +0000

    Creacion de choose.html como reemplazo de update.html, creacion de choose.pl

--- cgi-bin/choose.pl
+++ cgi-bin/choose.pl
@@ -21,22 +21,48 @@ BLOCK
     my $dsn = "DBI:MariaDB:database=pweb1;host=192.168.1.22";
     my $dbh = DBI->connect($dsn, $userDB, $passwordDB) or die("No se pudo conectar!");
 
-    my $query = "SELECT text FROM Articles WHERE owner = ? AND title = ?;";
+    my $query = "SELECT owner, title, text FROM Articles WHERE owner = ? AND title = ?;";
 
     my $sth = $dbh->prepare($query);
     $sth->execute($user,$title);
     
     my $row = $sth->fetchrow_hashref();
 
-    print $q->header('text/html');
-    my $textRaw = $row->{"text"};
+    my $ownerR = $row->{"owner"};
+    my $titleR = $row->{"title"};
+    my $textR = $row->{"text"};
 
     $sth->finish;
     $dbh->disconnect;
 
-    my $textHtml = convertor($textRaw);
-
-    print $textHtml;
+    print $q->header('text/html');
+print<<BLOCK;
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Editar pagina</title>
+</head>
+<body>
+    <header>
+        <h1>Edita tu pagina</h1>
+    </header>
+    <article>
+        <form action="update.pl">
+            <label for="1">Titulo: </label>
+            <input type="text" name="title" id="1" value="$titleR" readonly><br><br>
+            <label for="2">Texto: </label>
+            <textarea name="text" id="2" cols="30" rows="10">$textR</textarea><br>
+            <label for="3">Usuario: </label>
+            <input type="text" name="user" id="3" value="$ownerR" readonly><br><br>
+            <input type="submit" value="Editar">
+        </form>
+    </article>
+</body>
+</html>
+BLOCK
 }
 
 sub comprobar {
@@ -56,34 +82,4 @@ sub comprobar {
     $sthC->finish;
     $dbh->disconnect;
     return @row;
-}
-
-sub convertor {
-
-    my $markdown = $_[0];
-
-    $markdown =~ s/^#\s+(.*)$/<h1>$1<\/h1>/gm;
-
-    $markdown =~ s/^##\s+(.*)$/<h2>$1<\/h2>/gm;
-    
-    $markdown =~ s/^###\s+(.*)$/<h3>$1<\/h3>/gm;
-
-    $markdown =~ s/^####\s+(.*)$/<h4>$1<\/h4>/gm;
-
-    $markdown =~ s/^#####\s+(.*)$/<h5>$1<\/h5>/gm;
-
-    $markdown =~ s/^######\s+(.*)$/<h6>$1<\/h6>/gm;
-
-    $markdown =~ s/\*\*(.+?)\*\*/<p><strong>$1<\/strong><\/p>/g;
-
-    $markdown =~ s/\*(.+?)\*/<p><em>$1<\/em><\/p>/g;
-
-    $markdown =~ s/\*\*\*(.+?)\*\*\*/<p><strong><em>$1<\/em><\/strong><\/p>/g;
-
-    $markdown =~ s/~~(.+?)~~/<p><del>$1<\/del><\/p>/g;
-
-    $markdown =~ s/```(.+?)```/<pre style="background-color: rgb(230, 230, 230)"><code>$1<\/code><\/pre>/gs;
-
-    $markdown =~ s/\[(.+?)\]\((.+?)\)/<p><a href="$2">$1<\/a>.<\/p>/g;
-    return $markdown;
 }
